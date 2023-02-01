47b830dd5a0b87e9f61aaeffa469b35c6c29d9f9
Luana Silva <103971142+lunaE98@users.noreply.github.com>
2023-02-01 10:10:24 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href=" https://www.propelleraero.com/dirtmate/"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
