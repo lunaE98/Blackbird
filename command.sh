304b4dde4d291e025b6a442efd82b7fffd7ec790
Luana Silva <103971142+lunaE98@users.noreply.github.com>
2023-02-10 14:25:51 -0300

    The commands

---
+++ command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A list of commands 
+npx create-react-app my-app
+cd my-app
+npm start
+npm init react-app my-app
+npm init react-app
+cd my-app
+
+# return the exit status 
+exit $ret
